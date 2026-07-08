2e15d3274cfe95bb829a0b0a12dc3a01b3d8c963
Km-reshma <reshmak0811@gmail.com>
2026-07-08 16:56:52 +0530

    square star pattern

--- StarPattern.js
+++ StarPattern.js
@@ -181,6 +181,30 @@ Row   revser row                           Space          star
      
 */
 
+// square patterrn print...................................................
+/*
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
+
+let row =5;
+
+for(let i =1; i<=row ; i++){
+
+    let str= "";
+
+    for (let j =1; j<row ; j++){
+
+        str += "*";
+    }
+
+    console.log(str);
+
+}
+
 
 
  
