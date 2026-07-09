2331f60a5dfc955b03a121d45ffda24c31e8b778
Km-reshma <reshmak0811@gmail.com>
2026-07-09 22:50:26 +0530

    even and odd , check nymber

---
+++ decisionConditionPractice.js
@@ -0,0 +1,26 @@
+// ______________________Check if a number is positive, negative, or zero____________________________________________
+/*
+let num=-5;
+if(num>0){
+    console.log("Number is Positive");
+}
+else if(num<0){
+    console.log("Number is Negative");
+}
+else{
+    console.log("Number is zero");
+}
+// output : Number is Negative
+*/
+
+//_________________________ Check if a number is even or odd  ____________________________________
+
+let num=121;
+if(num%2==0){
+    console.log("Number is Even");
+}
+else{
+    console.log("Number is Odd");
+}                                                 // Number is Odd
+
+
