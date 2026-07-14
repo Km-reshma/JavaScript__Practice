38448ccbcbc08b2d44d002ac3570f8c340b2e173
Km-reshma <reshmak0811@gmail.com>
2026-07-14 23:04:32 +0530

    Check palindromeny using two pointer concept

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,5 @@
 // to check a string is palindrome or not?
-
+/*
 let str= "madam";
 rev_str ="";
 
@@ -17,3 +17,32 @@ if(str === rev_str){
      console.log("str is not palindrome");
     
  }                                              // str is plindrome
+*/
+
+ // while using two pointers
+
+ function isPalindrome(str){
+
+ let left  = 0 ;
+ let right = str.length-1;
+
+ while(left < right)
+    {
+
+    if(str[left] !== str[right])
+    {
+
+      return false;
+    }
+
+     left++;
+     right--;
+    } 
+
+    return true;
+
+}
+
+console.log(isPalindrome("madam"));        // return true
+console.log(isPalindrome("toot"));        // return true
+console.log(isPalindrome("ten"));        // return false
